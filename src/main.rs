3a86ac323617e21ec706f3260ed9c5110d93cb0e
a6.webm <46289515+a6-webm@users.noreply.github.com>
2023-07-29 22:26:59 +0100

    can make sound

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-use std::{ffi::CString, time::Instant};
+use std::{
+    ffi::CString,
+    iter::zip,
+    sync::{Arc, Mutex},
+    thread::sleep,
+    time::Duration,
+};
+
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    SampleFormat, SampleRate, Stream,
+};
 
 extern "C" {
     fn vtlInitialize(speakerFileName: *const i8) -> i32;
@@ -15,9 +26,55 @@ extern "C" {
 
 struct VTLParam {
     name: String,
-    val: f64,
+    start_val: f64,
     min: f64,
     max: f64,
+}
+
+fn only_vals(vec: &Vec<VTLParam>) -> Vec<f64> {
+    vec.iter().map(|p| p.start_val).collect()
+}
+
+fn audio_setup(mx_data: Arc<Mutex<(Vec<f64>, Vec<f64>)>>) -> Stream {
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("no output device available");
+    let supported_configs_range = device
+        .supported_output_configs()
+        .expect("error while querying configs");
+    let supported_config = {
+        let mut sup_conf = None;
+        for conf in supported_configs_range.into_iter() {
+            if conf.channels() == 1 && conf.sample_format() == SampleFormat::F32 {
+                sup_conf = Some(conf.with_sample_rate(SampleRate(44000)));
+            }
+        }
+        sup_conf.expect("no supported config")
+    };
+    let err_fn = |err| eprintln!("an error occurred on the output audio stream: {}", err);
+    let config = supported_config.into();
+    device
+        .build_output_stream(
+            &config,
+            move |data: &mut [f32], _| unsafe {
+                let mut audio: Vec<f64> = vec![0.0; data.len()];
+                let mut params = mx_data.lock().unwrap();
+                vtlSynthesisAddTract(
+                    data.len() as i32,
+                    audio.as_mut_ptr(),
+                    params.0.as_mut_ptr(),
+                    params.1.as_mut_ptr(),
+                );
+                drop(params);
+                for (d, s) in zip(data.iter_mut(), audio.iter().cloned()) {
+                    *d = s as f32;
+                }
+            },
+            err_fn,
+            None,
+        )
+        .unwrap()
 }
 
 fn main() {
@@ -26,115 +83,115 @@ fn main() {
             name: "HX".to_owned(),
             min: 0.000,
             max: 1.000,
-            val: 1.000,
+            start_val: 1.000,
         },
         VTLParam {
             name: "HY".to_owned(),
             min: -6.000,
             max: -3.500,
-            val: -4.750,
+            start_val: -4.750,
         },
         VTLParam {
             name: "JX".to_owned(),
             min: -0.500,
             max: 0.000,
-            val: 0.000,
+            start_val: 0.000,
         },
         VTLParam {
             name: "JA".to_owned(),
             min: -7.000,
             max: 0.000,
-            val: -2.000,
+            start_val: -2.000,
         },
         VTLParam {
             name: "LP".to_owned(),
             min: -1.000,
             max: 1.000,
-            val: -0.070,
+            start_val: -0.070,
         },
         VTLParam {
             name: "LD".to_owned(),
             min: -2.000,
             max: 4.000,
-            val: 0.950,
+            start_val: 0.950,
         },
         VTLParam {
             name: "VS".to_owned(),
             min: 0.000,
             max: 1.000,
-            val: 0.000,
+            start_val: 0.000,
         },
         VTLParam {
             name: "VO".to_owned(),
             min: -0.100,
             max: 1.000,
-            val: -0.100,
+            start_val: -0.100,
         },
         VTLParam {
             name: "TCX".to_owned(),
             min: -3.000,
             max: 4.000,
-            val: -0.400,
+            start_val: -0.400,
         },
         VTLParam {
             name: "TCY".to_owned(),
             min: -3.000,
             max: 1.000,
-            val: -1.460,
+            start_val: -1.460,
         },
         VTLParam {
             name: "TTX".to_owned(),
             min: 1.500,
             max: 5.500,
-            val: 3.500,
+            start_val: 3.500,
         },
         VTLParam {
             name: "TTY".to_owned(),
             min: -3.000,
             max: 2.500,
-            val: -1.000,
+            start_val: -1.000,
         },
         VTLParam {
             name: "TBX".to_owned(),
             min: -3.000,
             max: 4.000,
-            val: 2.000,
+            start_val: 2.000,
         },
         VTLParam {
             name: "TBY".to_owned(),
             min: -3.000,
             max: 5.000,
-            val: 0.500,
+            start_val: 0.500,
         },
         VTLParam {
             name: "TRX".to_owned(),
             min: -4.000,
             max: 2.000,
-            val: 0.000,
+            start_val: 0.000,
         },
         VTLParam {
             name: "TRY".to_owned(),
             min: -6.000,
             max: 0.000,
-            val: 0.000,
+            start_val: 0.000,
         },
         VTLParam {
             name: "TS1".to_owned(),
             min: 0.000,
             max: 1.000,
-            val: 0.000,
+            start_val: 0.000,
         },
         VTLParam {
             name: "TS2".to_owned(),
             min: 0.000,
             max: 1.000,
-            val: 0.000,
+            start_val: 0.000,
         },
         VTLParam {
             name: "TS3".to_owned(),
             min: -1.000,
             max: 1.000,
-            val: 0.000,
+            start_val: 0.000,
         },
     ];
 
@@ -143,78 +200,77 @@ fn main() {
             name: "F0".to_owned(),
             min: 40.000000,
             max: 600.000000,
-            val: 120.000000,
+            start_val: 120.000000,
         },
         VTLParam {
             name: "PR".to_owned(),
             min: 0.000000,
             max: 20000.000000,
-            val: 8000.000000,
+            start_val: 8000.000000,
         },
         VTLParam {
             name: "XB".to_owned(),
             min: -0.050000,
             max: 0.300000,
-            val: 0.010000,
+            start_val: 0.010000,
         },
         VTLParam {
             name: "XT".to_owned(),
             min: -0.050000,
             max: 0.300000,
-            val: 0.020000,
+            start_val: 0.020000,
         },
         VTLParam {
             name: "CA".to_owned(),
             min: -0.250000,
             max: 0.250000,
-            val: 0.050000,
+            start_val: 0.050000,
         },
         VTLParam {
             name: "PL".to_owned(),
             min: 0.000000,
             max: 3.141500,
-            val: 1.220000,
+            start_val: 1.220000,
         },
         VTLParam {
             name: "RA".to_owned(),
             min: -1.000000,
             max: 1.000000,
-            val: 1.000000,
+            start_val: 1.000000,
         },
         VTLParam {
             name: "DP".to_owned(),
             min: 0.000000,
             max: 1.000000,
-            val: 0.050000,
+            start_val: 0.050000,
         },
         VTLParam {
             name: "PS".to_owned(),
             min: -0.500000,
             max: 0.500000,
-            val: 0.000000,
+            start_val: 0.000000,
         },
         VTLParam {
             name: "FL".to_owned(),
             min: 0.000000,
             max: 100.000000,
-            val: 25.000000,
+            start_val: 25.000000,
         },
         VTLParam {
             name: "AS".to_owned(),
             min: -40.000000,
             max: 0.000000,
-            val: -10.000000,
+            start_val: -10.000000,
         },
     ];
-
-    unsafe fn to_raw_vals(vec: &Vec<VTLParam>) -> *mut f64 {
-        let mut out: Vec<f64> = vec.iter().map(|p| p.val).collect();
-        return out.as_mut_ptr();
-    }
 
     let speaker = CString::new("./res/JD3.speaker").unwrap();
     let num_samples: usize = 220;
     let mut audio: Vec<f64> = vec![0.0; num_samples];
+    let throat_vals: (Vec<f64>, Vec<f64>) = (
+        tract_params.iter().map(|p| p.start_val).collect(),
+        glottis_params.iter().map(|p| p.start_val).collect(),
+    );
     unsafe {
         if vtlInitialize(speaker.as_ptr()) == 1 {
             panic!("failed to load speaker file")
@@ -224,18 +280,22 @@ fn main() {
         vtlSynthesisAddTract(
             0,
             audio.as_mut_ptr(),
-            to_raw_vals(&tract_params),
-            to_raw_vals(&glottis_params),
+            only_vals(&tract_params).as_mut_ptr(),
+            only_vals(&glottis_params).as_mut_ptr(),
         );
-        let timer = Instant::now();
-        vtlSynthesisAddTract(
-            num_samples as i32,
-            audio.as_mut_ptr(),
-            to_raw_vals(&tract_params),
-            to_raw_vals(&glottis_params),
-        );
-        let elapsed = timer.elapsed().as_secs_f64();
-        println!("Took {elapsed} seconds");
+    }
+    let mx_data = Arc::new(Mutex::new(throat_vals));
+    let stream = audio_setup(mx_data.clone());
+    stream.play().unwrap();
+    for i in 0..100 {
+        let mut vals = mx_data.lock().unwrap();
+        println!("{i}");
+        vals.1[1] += 200.0;
+        drop(vals);
+        sleep(Duration::from_millis(20));
+    }
+    drop(stream);
+    unsafe {
         vtlClose();
     }
 }
