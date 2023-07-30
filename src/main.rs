1f5bc352a34e31e06b9a2b36debbae0d94cfebb2
a6.webm <46289515+a6-webm@users.noreply.github.com>
2023-07-30 12:16:18 +0100

    put VTL api calls in a safe wrapper struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     ffi::CString,
     iter::zip,
-    ptr::null_mut,
     sync::{Arc, Mutex},
     thread::sleep,
     time::Duration,
@@ -11,22 +10,9 @@ use cpal::{
     traits::{DeviceTrait, HostTrait, StreamTrait},
     SampleFormat, SampleRate, Stream,
 };
-use hand2talk::{GLOTTIS_PARAMS, TRACT_PARAMS};
+use hand2talk::vtl::{VTLApi, GLOTTIS_PARAMS, TRACT_PARAMS};
 
-extern "C" {
-    fn vtlInitialize(speakerFileName: *const i8) -> i32;
-    fn vtlClose() -> i32;
-    fn vtlCalcTongueRootAutomatically(automaticCalculation: bool) -> i32;
-    fn vtlSynthesisReset() -> i32;
-    fn vtlSynthesisAddTract(
-        numNewSamples: i32,
-        audio: *mut f64,
-        tractParams: *mut f64,
-        glottisParams: *mut f64,
-    ) -> i32;
-}
-
-fn audio_setup(mx_data: Arc<Mutex<(Vec<f64>, Vec<f64>)>>) -> Stream {
+fn audio_setup(mut vtl_api: VTLApi, mx_data: Arc<Mutex<(Vec<f64>, Vec<f64>)>>) -> Stream {
     let host = cpal::default_host();
     let device = host
         .default_output_device()
@@ -48,15 +34,9 @@ fn audio_setup(mx_data: Arc<Mutex<(Vec<f64>, Vec<f64>)>>) -> Stream {
     device
         .build_output_stream(
             &config,
-            move |data: &mut [f32], _| unsafe {
-                let mut audio: Vec<f64> = vec![0.0; data.len()];
-                let mut params = mx_data.lock().unwrap();
-                vtlSynthesisAddTract(
-                    data.len() as i32,
-                    audio.as_mut_ptr(),
-                    params.0.as_mut_ptr(),
-                    params.1.as_mut_ptr(),
-                );
+            move |data: &mut [f32], _| {
+                let params = mx_data.lock().unwrap();
+                let audio = vtl_api.add_tract(data.len(), &params.0, &params.1);
                 drop(params);
                 for (d, s) in zip(data.iter_mut(), audio.iter().cloned()) {
                     *d = s as f32;
@@ -70,25 +50,15 @@ fn audio_setup(mx_data: Arc<Mutex<(Vec<f64>, Vec<f64>)>>) -> Stream {
 
 fn main() {
     let speaker = CString::new("./res/JD3.speaker").unwrap();
-    let mut throat_vals: (Vec<f64>, Vec<f64>) = (
+    let mut vtl_api = VTLApi::new(speaker).expect("failed to load speaker file");
+    vtl_api.auto_calc_tr(true);
+    let throat_vals: (Vec<f64>, Vec<f64>) = (
         TRACT_PARAMS.iter().map(|p| p.start_val).collect(),
         GLOTTIS_PARAMS.iter().map(|p| p.start_val).collect(),
     );
-    unsafe {
-        if vtlInitialize(speaker.as_ptr()) == 1 {
-            panic!("failed to load speaker file")
-        };
-        vtlCalcTongueRootAutomatically(true);
-        vtlSynthesisReset();
-        vtlSynthesisAddTract(
-            0,
-            null_mut(),
-            throat_vals.0.as_mut_ptr(),
-            throat_vals.1.as_mut_ptr(),
-        );
-    }
+    vtl_api.reset(&throat_vals.0, &throat_vals.1);
     let mx_data = Arc::new(Mutex::new(throat_vals));
-    let stream = audio_setup(mx_data.clone());
+    let stream = audio_setup(vtl_api, mx_data.clone());
     stream.play().unwrap();
 
     for i in 0..100 {
@@ -97,10 +67,5 @@ fn main() {
         vals.1[1] += 200.0;
         drop(vals);
         sleep(Duration::from_millis(20));
-    }
-
-    drop(stream);
-    unsafe {
-        vtlClose();
     }
 }
